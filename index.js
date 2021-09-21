83f4f2cb3fbcab8f0e0d60c86b8f0c003a87bfea
Prajwal Gaikwad <74761056+PrajwalG7@users.noreply.github.com>
2021-09-21 19:53:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //If user adds a note add it to local storage
-
+ 
 let addBtn = document.getElementById("addBtn");
+showNotes();
+
 addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
   let addTitle = document.getElementById("addTitle");
@@ -14,14 +16,17 @@ addBtn.addEventListener("click", function (e) {
     title: addTitle.value,
     text: addTxt.value,
   };
+
   notesObj.push(myObj);
+
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
   addTitle.value = "";
- // console.log(notesObj);
+  console.log(notesObj);
 
   showNotes();
 });
+
 function showNotes() {
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -30,19 +35,23 @@ function showNotes() {
     notesObj = JSON.parse(notes);
   }
   let html = "";
+
   notesObj.forEach(function (element, index) {
-    html =
-      html +
-      `
-      <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+    
+      html =
+        html +
+        `
+      <div class="noteCard my-2 mx-2 card" style="width: 18rem;" >
         <div class="card-body">
-          <h5 class="card-title"> Note ${index + 1}: ${element.title}</h5>
+          <h5 class="card-title"> Title ${index + 1}: ${element.title}</h5>
           <p class="card-text">${element.text} </p>
           <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">
             Delete Note
           </button>
         </div>
       </div>`;
+     
+    
   });
   let notesElm = document.getElementById("notes");
   if (notesObj.length != 0) {
@@ -54,10 +63,9 @@ function showNotes() {
 
 //function to delete node
 function deleteNote(index) {
-  // console.log("deleting note", index);
   let notes = localStorage.getItem("notes");
   if (notes == null) {
-    // notesObj = [];
+    notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
@@ -70,13 +78,12 @@ function deleteNote(index) {
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function () {
   let inputVal = search.value;
-
- // console.log("event fired", inputVal);
   let noteCards = document.getElementsByClassName("noteCard");
   Array.from(noteCards).forEach(function (element) {
-    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    let cardTxt = element.getElementsByTagName("h5")[0].innerText;
     if (cardTxt.includes(inputVal)) {
       element.style.display = "block";
+      element.style.display = "";
     } else {
       element.style.display = "none";
     }
